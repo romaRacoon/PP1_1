6a468508577f2f56e88d9f54cebcfb4b7645d445
romaRacoon <roma1808@inbox.ru>
2022-06-16 19:59:00 +0300

    make connection to database

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final String NAME = "root";
+    private final String PASSWORD = "rickandmortyroman1M";
+    private final String URL = "jdbc:mysql://localhost:3306/testwirhutf";
+
+    private static Connection connection;
+    private static Statement statement;
+
+    private void createStatement() {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void installConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, NAME, PASSWORD);
+            createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
