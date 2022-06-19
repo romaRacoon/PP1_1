b4c73d534da57c099e1d3d44b33afce3c1b7fc81
romaRacoon <roma1808@inbox.ru>
2022-06-19 22:02:00 +0300

    Start make second half of task

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,10 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.*;
 
 public class Util {
@@ -7,6 +12,7 @@ public class Util {
     private static final String NAME = "root";
     private static final String PASSWORD = "rickandmortyroman1M";
     private static final String URL = "jdbc:mysql://localhost:3306/testwirhutf";
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         Connection connection = null;
@@ -18,5 +24,20 @@ public class Util {
         }
 
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(User.class);
+                StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder();
+                sessionFactory = configuration.buildSessionFactory(standardServiceRegistryBuilder.build());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sessionFactory;
     }
 }
