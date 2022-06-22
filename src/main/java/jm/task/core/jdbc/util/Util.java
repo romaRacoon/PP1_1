d973ed33312b85d04474013a2c8e0002dee1e371
romaRacoon <roma1808@inbox.ru>
2022-06-23 00:37:07 +0300

    b

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,8 @@
 package jm.task.core.jdbc.util;
 
-import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
 
 import java.sql.*;
-import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
